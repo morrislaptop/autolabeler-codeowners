fe0ad579612a833fe25c342caa9ce1728c134571
Craig Morris <>
2024-06-24 23:04:14 +1000

    wot

--- __tests__/getCodeOwnersFromPaths.test.ts
+++ __tests__/getCodeOwnersFromPaths.test.ts
@@ -2,7 +2,7 @@ import { getCodeOwnersFromPaths } from '../src/getCodeOwnersFromPaths'
 
 // these tests use the CODEOWNERS file located in .github/CODEOWNERS
 test('* picks @gatsby', async () => {
-  expect.assertions(2)
+  expect.assertions(3)
   const inplist: string[] = ['__tests__/getCodeOwners.test.ts']
   const owners: Set<string> = await getCodeOwnersFromPaths(inplist)
   expect(owners).toBeDefined()
@@ -11,7 +11,7 @@ test('* picks @gatsby', async () => {
 })
 
 test('src picks @org-teamA but NOT @gatsby', async () => {
-  expect.assertions(2)
+  expect.assertions(3)
   const inplist: string[] = ['src/getCodeOwners.ts']
   const owners: Set<string> = await getCodeOwnersFromPaths(inplist)
   expect(owners).toBeDefined()
@@ -20,7 +20,7 @@ test('src picks @org-teamA but NOT @gatsby', async () => {
 })
 
 test('multiple codeowners are all chosen', async () => {
-  expect.assertions(2)
+  expect.assertions(4)
   const inplist: string[] = ['src/b/c/d.ts']
   const owners: Set<string> = await getCodeOwnersFromPaths(inplist)
   expect(owners).toBeDefined()
